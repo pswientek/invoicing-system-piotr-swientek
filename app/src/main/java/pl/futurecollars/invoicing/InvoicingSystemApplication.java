2f9e6e55d1f09a28d720e203e408c1431a113c77
Piotr Swientek <swientekpiot@gmail.com>
2022-08-04 15:48:41 +0200

    add source code for task 4

---
+++ app/src/main/java/pl/futurecollars/invoicing/InvoicingSystemApplication.java
@@ -0,0 +1,12 @@
+package pl.futurecollars.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicingSystemApplication.class, args);
+    }
+}
