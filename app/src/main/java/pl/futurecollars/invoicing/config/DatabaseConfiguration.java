c4fc185b0b428b1b7ffabc4da81e508bdc833794
Piotr Swientek <swientekpiot@gmail.com>
2022-08-16 17:55:30 +0200

    add solutions to task 7

--- app/src/main/java/pl/futurecollars/invoicing/config/DatabaseConfiguration.java
+++ app/src/main/java/pl/futurecollars/invoicing/config/DatabaseConfiguration.java
@@ -1,37 +1,51 @@
 package pl.futurecollars.invoicing.config;
 
 import java.io.IOException;
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.db.memory.FileBasedDatabase;
+import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
 import pl.futurecollars.invoicing.service.FileService;
 import pl.futurecollars.invoicing.service.IdService;
 import pl.futurecollars.invoicing.service.JsonService;
 
+@Slf4j
 @Configuration
 public class DatabaseConfiguration {
 
-    private static final String ID_FILE_NAME = "ids.txt";
-    private static final String INVOICES_FILE_NAME = "dbFile.txt";
+    @Value("${invoicing-system.database.id.file}")
+    public String idFile;
+    @Value("${invoicing-system.database.invoices.file}")
+    public String invoicesFile;
 
     @Bean
     public IdService idService() throws IOException {
-        FileService fileService = new FileService(ID_FILE_NAME);
+        FileService fileService = new FileService(idFile);
         return new IdService(fileService);
     }
 
     @Bean
     public FileService fileService() throws IOException {
-        return new FileService(INVOICES_FILE_NAME);
+        return new FileService(invoicesFile);
     }
 
-    @SneakyThrows
+    @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "file")
     @Bean
-    public Database fileBasedDatabase() {
-        IdService idService = new IdService(new FileService(ID_FILE_NAME));
-        return new FileBasedDatabase(idService, new FileService(INVOICES_FILE_NAME), new FileService(ID_FILE_NAME), new JsonService());
+    public Database fileBasedDatabase() throws IOException {
+        log.debug("File based database was created");
+        IdService idService = new IdService(new FileService(idFile));
+        return new FileBasedDatabase(idService, new FileService(invoicesFile), new FileService(idFile), new JsonService());
+    }
+
+    @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "memory")
+    @Bean
+    public Database inMemoryDatabase() {
+        log.debug("In memory database was created");
+        return new InMemoryDatabase();
     }
 
 }
