7f2022c3c917a302d54c10604bfc452bb0148469
pswientek <99757305+pswientek@users.noreply.github.com>
2022-08-24 23:10:06 +0200

    Task 08 beta (#14)
    * update gradlew-version to 7.5.1
    
    * add solutions to task 7
    
    * add solutions to task 8
    
    Co-authored-by: Piotr Swientek <swientekpiot@gmail.com>

--- app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,52 +1,41 @@
 package pl.futurecollars.invoicing.controller;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.RestService;
 
 @RestController
-@RequestMapping("invoices")
-public class InvoiceController {
+@AllArgsConstructor
+public class InvoiceController implements InvoiceApi {
 
-    @Autowired
     private final RestService restService;
 
-    @Autowired
-    public InvoiceController(RestService restService) {
-        this.restService = restService;
-    }
-
-    @PostMapping
+    @Override
     public int add(@RequestBody Invoice invoice) {
         return restService.save(invoice);
     }
 
-    @GetMapping(produces = { "application/json;charset=UTF-8" })
+    @Override
     public List<Invoice> getAll() {
         return restService.getAll();
     }
 
-    @GetMapping("/{id}")
+    @Override
     public ResponseEntity<Invoice> getById(@PathVariable int id) {
         return restService.getById(id);
     }
 
-    @PutMapping("/{id}")
+    @Override
     public ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice) {
         return restService.update(id, invoice);
     }
 
-    @DeleteMapping("/{id}")
+    @Override
     public ResponseEntity<?> deleteById(@PathVariable int id) {
         return restService.deleteById(id);
     }
